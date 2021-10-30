e09205afb63d150c1b7240d17a7a0174dbfd7477
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2021-10-30 16:19:13 +0200

    Refactored challenge 5

--- challenge_5.py
+++ challenge_5.py
@@ -4,21 +4,37 @@
 
 import itertools
 
-def repeating_key_xor(input_bytes, key):
-    # Loop trough each byte of the input and iterate over the key in a cycle (so ICEICEICEICE...)
+def repeating_key_xor(input: bytes, key: bytes) -> str:
+    """Encrypt the given input with repeating-key XOR
+    
+    Args:
+        input: input to encrypt
+        key: key used during encryption
+    
+    Returns:
+        str: hex encoded string with the encrypted input 
+    """
+    # Loop trough each byte of the input and iterate over the key in a cycle (so for key ICE: ICEICEICEICE...)
     result = []
-    for byte, key_byte in zip(input_bytes, itertools.cycle(key)):
+    for byte, key_byte in zip(input, itertools.cycle(key)):
         print(f'Encrypting {chr(byte)} with {chr(key_byte)}')
         # Use the XOR operator on the two bytes
         xor_byte = byte ^ key_byte
         # Store the resulting byte in an array
         result.append(xor_byte)
-    # return the result in hexadecimal  
+    # Return the result in hexadecimal  
     return bytes(result).hex()
 
-if __name__ == "__main__":
-    key = b'ICE'
-    input_bytes = b"Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
 
-    print(repeating_key_xor(input_bytes, key))
-    print('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f')
+def test_repeating_key_xor():
+    key = b'ICE'
+    input = b"Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
+
+    result = repeating_key_xor(input, key)
+
+    assert result == '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20690a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+
+    print('Passed')
+
+if __name__ == "__main__":
+    test_repeating_key_xor()    
